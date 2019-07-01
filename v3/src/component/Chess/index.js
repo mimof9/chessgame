e3edb809089ee16593ffa00b3a220be659a6ee7b
mimof9 <1509945419@qq.com>
2019-07-01 13:01:46 +0800

    组件化

--- v3/src/component/Chess/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Chess extends Component {
-    render() {
-        return (
-            <button className={}></button>
-        )
-    }
-}
